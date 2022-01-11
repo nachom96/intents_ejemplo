5282c372e95c7088cca9ce36a5132094ffbeafd5
Nacho <nacho_085@hotmail.com>
2022-01-11 09:33:02 +0100

    Parte 1
    Comentada con los pasos a seguir numéricamente.

--- app/src/main/java/com/example/intentsejemplo/MainActivity.kt
+++ app/src/main/java/com/example/intentsejemplo/MainActivity.kt
@@ -2,10 +2,97 @@ package com.example.intentsejemplo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.intentsejemplo.databinding.MainActivityBinding
+import com.example.intentsejemplo.utils.hideSoftKeyboard
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: MainActivityBinding
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupViews()
     }
+
+    // 9º
+    private fun setupViews() {
+        binding.txtAge.setOnEditorActionListener { _, _, _ ->
+            btnShowOnClick()
+            true
+        }
+        binding.btnShow.setOnClickListener { btnShowOnClick() }
+    }
+
+    // 7º
+    private fun btnShowOnClick() {
+        val name = binding.txtName.text.toString()
+        val age = binding.txtAge.text.toString()
+        if (isValidForm(name, age)){
+            binding.txtName.hideSoftKeyboard()
+            navigateToDetailActivity(name, age.toInt())
+        }
+    }
+
+    // 5º Si el nombre o la edad son incorrectas, se pone el foco en ellas
+    private fun isValidForm(name: String, ageText: String) : Boolean{
+        if (!validateName(name)) {
+            binding.txtName.requestFocus()
+            return false
+        }
+        if (!validateAge(ageText)) {
+            binding.txtAge.requestFocus()
+            return false
+        }
+        return true
+    }
+
+    // 4º
+    private fun validateName(name: String): Boolean{
+        return if (isValidName(name)) {
+            binding.txtName.error = null
+            true
+        } else {
+            binding.txtName.error = getString(R.string.main_invalid_name)
+            false
+        }
+    }
+
+    // 3º Se le pasa la edad. Si es válida el mensaje de error desaparece, si no se muestra.
+    private fun validateAge(ageText: String): Boolean{
+        return if (isValidAge(ageText)){
+            binding.txtAge.error = null
+            true
+        } else{
+            binding.txtAge.error = getString(R.string.main_invalid_age)
+            false
+        }
+    }
+
+    // 2º
+    private fun isValidName(name: String): Boolean = name.isNotEmpty()
+
+    // 1º
+    private fun isValidAge(ageText: String): Boolean{
+        // Devuelve un int o un null si no se puede transformar a int. El elvis sirve para retornar false en caso de ser null.
+        val age = ageText.toIntOrNull() ?: return false
+        // validRange es el valor entre el que puede estar contenido la edad pasada.
+        // contains comprueba que el valor introducido está entre el validRange
+        val validRange = 1..130
+        return validRange.contains(age)
+
+    }
+
+    // 8º
+    private fun navigateToDetailActivity(name: String, age: Int){
+        binding.btnShow.hideSoftKeyboard()
+        // TODO: Navigate to DetailActivity -- Tema 5 -> Proyecto ExplicitIntent
+    }
+
+
+
+
 }
